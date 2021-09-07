e350f96504f50bc177321dca580ec226a396bb8e
이충희 <edgar@ichunghuiui-MacBookPro.local>
2021-09-07 17:37:13 +0900

    chap 2. section 10 prac- domain class created.
    실습 - 영역 객체 생성

--- src/main/java/com/ezen/webstore/config/DispatcherServletInitializer.java
+++ src/main/java/com/ezen/webstore/config/DispatcherServletInitializer.java
@@ -17,6 +17,6 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
 
 	@Override
 	protected String[] getServletMappings() {
-		return new String[] { "/app/*" };
+		return new String[] { "/" };
 	}
 }
