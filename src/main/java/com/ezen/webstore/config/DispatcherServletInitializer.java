74bac180ef2bf54f9bd0a179128612e4f395b65d
이충희 <edgar@ichunghuiui-MacBookPro.local>
2021-09-07 16:15:39 +0900

    제 2장 2.5 View resolvers done

--- src/main/java/com/ezen/webstore/config/DispatcherServletInitializer.java
+++ src/main/java/com/ezen/webstore/config/DispatcherServletInitializer.java
@@ -17,6 +17,6 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
 
 	@Override
 	protected String[] getServletMappings() {
-		return new String[] { "/" };
+		return new String[] { "/app/*" };
 	}
 }
